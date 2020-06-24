1b1255e9e62fca00d39615fcea773aa9ef9ef154
Alex Chip <alexchip32@gmail.com>
2020-06-23 18:32:50 -0600

    Gestionando Errores

--- pages/channel.js
+++ pages/channel.js
@@ -1,10 +1,14 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import Error from 'next/error'
 import Layout from '../components/Layout'
 import BannerChannel from '../components/BannerChannel'
 import Series from '../components/Series'
 import AudioClips from '../components/AudioClips'
 
-const Channel = ({ channel, audioClips, series }) => {
+const Channel = ({ channel, audioClips, series, statusCode }) => {
+  if (statusCode !== 200 ) {
+    return <Error statusCode={ statusCode }/>
+  }
   return(
     <Layout title={channel.title}>
       <BannerChannel channel ={ channel }/>
@@ -17,22 +21,31 @@ const Channel = ({ channel, audioClips, series }) => {
 Channel.getInitialProps = async ({ query }) => {
   let idChannel = query.id
 
-  let [reqChannel, reqSeries, reqAudios] = await Promise.all([
-    fetch(`https://api.audioboom.com/channels/${idChannel}`),
-    fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
-    fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
-  ])
+  try {
+    let [reqChannel, reqSeries, reqAudios] = await Promise.all([
+      fetch(`https://api.audioboom.com/channels/${idChannel}`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+    ])
 
-  let dataChannel = await reqChannel.json()
-  let channel = dataChannel.body.channel
+    if(reqChannel.status >= 400) {
+      res.statusCode = reqChannel.status
+    return { channel: null, audioClips: null, series: null, statusCode: reqChannel.status}
+    }
 
-  let dataAudios = await reqAudios.json()
-  let audioClips = dataAudios.body.audio_clips
+    let dataChannel = await reqChannel.json()
+    let channel = dataChannel.body.channel
 
-  let dataSeries = await reqSeries.json()
-  let series = dataSeries.body.channels
+    let dataAudios = await reqAudios.json()
+    let audioClips = dataAudios.body.audio_clips
 
-  return { channel, audioClips, series }
+    let dataSeries = await reqSeries.json()
+    let series = dataSeries.body.channels
+
+    return { channel, audioClips, series, statusCode: 200 }
+  } catch(err){
+    return { channel: null, audioClips: null, series: null, statusCode: 503}
+  }
 }
 
 export default Channel
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import 'isomorphic-fetch'
 import Layout from '../components/Layout'
 import ChannelGrid from '../components/ChannelGrid'
+import Error from 'next/error'
 
-const Home = ({ channels }) => {
+const Home = ({ channels, statusCode }) => {
+  if (statusCode !== 200 ) {
+    return <Error statusCode={ statusCode }/>
+  }
   return(
     <Layout title='Platzi-Podcasts'>
       <ChannelGrid channels={ channels } />
@@ -10,10 +14,15 @@ const Home = ({ channels }) => {
   )
 }
 
-Home.getInitialProps = async() => {
-  let req = await fetch('https://api.audioboom.com/channels/recommended')
-  let { body: channels } = await req.json()
-  return { channels }
+Home.getInitialProps = async ({ res }) => {
+  try {
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let { body: channels } = await req.json()
+    return { channels, statusCode: 200 }
+  } catch(err) {
+    res.statusCode = 503
+    return { channels: null, statusCode: 503}
+  }
 }
 
 export default Home
